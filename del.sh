56f8baca2ae1857000972d5231d211306cc8cc83
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2020-12-11 14:06:39 +0600

    finally??
    Signed-off-by: Tamjid Ahmed <mail.to.tamjid@gmail.com>

--- del.sh
+++ del.sh
@@ -1,11 +1,10 @@
-URL="http://udapeople-ae5893d.s3-website-us-west-2.amazonaws.com"
-echo $URL
-echo $(curl -s ${URL} | grep "Welcome")
-if curl -s ${URL} | grep "Welcome"
-then
-    echo "OK"
-  exit 0
-else
-    echo "NO"
-  exit 1
+export OldWorkflowID=$(curl -H "token: tamjid" --request GET \
+                      https://api.memstash.io/values/persist_workflow_id)
+echo $OldWorkflowID
+
+if [ "${OldWorkflowID}" != "" ]
+    then
+        echo "Delere"
+    else
+        echo "Do Nothing"
 fi
